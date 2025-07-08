80cc1d1ab6be229180a6ccd441a860087f7d5a10
Teutorigos <popint94@yahoo.com>
2025-07-08 06:36:05 -0400

    added operations

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,46 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+
+    cart.forEach(item => {
+        const quantity = item.quantity;
+        const cost = parseFloat(item.cost.substring(1)); // remove "$" and convert to number
+        total += quantity * cost;
+    });
+
+    return total;      
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping();
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+   };
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+  if (item.quantity > 1) {
+   dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+  }
+  else{
+   dispatch(removeQuantity({ name: item.name })); 
   };
 
   const handleRemove = (item) => {
+    dispatch(removeQuantity({ name: item.name })); 
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let value = parseFloat(item.cost.substring(1));
+    return item.quantity * value;
   };
 
   return (
